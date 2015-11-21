2706717fa21392817e3b69ee029949c6ebbd5ff6
vineethakagitha <vineetha.muni@gmail.com>
2015-11-21 01:54:20 -0800

    Submit Commit

--- src/NumbertoStr.cpp
+++ src/NumbertoStr.cpp
@@ -19,7 +19,47 @@ NOTES: Don't create new string.
 
 #include <stdio.h>
 
-
 void number_to_str(float number, char *str,int afterdecimal){
-	
+	int i, d, j, p = 0, inte = 1,num;
+	char temp;
+	if (number < 0)
+	{
+		number = -number; p = 1; str[0] = '-';
+	}
+	for (i = 0; i < afterdecimal; i++)
+	{
+		inte = inte * 10;
+	}
+	number = number*inte;
+	num = number;
+	for (i = p; num!=0; i++)
+	{
+		d = num % 10;
+		if (afterdecimal && (afterdecimal+p)== (i))
+		{
+			str[i] = '.';
+			continue;
+		}
+		str[i] = d+'0';
+		num = num / 10;
+	}
+	str[i] = '\0';
+	if (p)
+	{
+		for (j = p; j <= i / 2; j++)
+		{
+			temp = str[j];
+			str[j] = str[i - j];
+			str[i - j] = temp;
+		}
+	}
+	else{
+		for (j = 0; j < i / 2; j++)
+		{
+			temp = str[j];
+			str[j] = str[i - (j)-1];
+			str[i - (j)-1] = temp;
+		}
+	}
 }
+
--- src/removeSpaces.cpp
+++ src/removeSpaces.cpp
@@ -12,5 +12,21 @@ NOTES: Don't create new string.
 */
 
 char removeSpaces(char *str) {
+	int i,j;
+	if (str && str!="")
+	{
+		for (i = 0; str[i]; i++)
+		{
+			if (str[i] == ' ')
+			{
+				for (j = i; str[j+1]; j++)
+				{
+					str[j] = str[j + 1];
+				}
+				str[j] = '\0';
+			}
+		}
+		return *str;
+	}
 	return '\0';
 }
