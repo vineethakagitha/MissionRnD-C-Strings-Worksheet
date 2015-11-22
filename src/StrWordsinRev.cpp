2e07ecfc2478b8a9cdce5bc389ebdfdee145430b
vineethakagitha <vineetha.muni@gmail.com>
2015-11-22 07:44:08 -0800

    Submit Commit

--- src/StrWordsinRev.cpp
+++ src/StrWordsinRev.cpp
@@ -11,7 +11,31 @@ NOTES: Don't create new string.
 */
 #include <Stdio.h>
 #include <string.h>
-
+#include<stdlib.h>
 void str_words_in_rev(char *input, int len){
-	
+	int i, j,n,sh=0,l;
+	char *temp;
+	temp = (char*)malloc(sizeof(char)*len);
+	n = len;
+	j = len - 1;
+	for (i = 0; i < len; i++)
+		temp[i] = input[i];
+	while (j > 0)
+	{
+		for (j = n - 1,l=0; input[j] != ' ' &&  j>0; j--);
+		if (j == 0)
+			l = -1;
+		for (i = j +l+1; input[i] != ' ' && input[i]; i++,sh++)
+		{
+			temp[sh] = input[i];
+		}
+		for (n = j; input[n] == ' ' && n>0; n--)
+		{
+			temp[sh] = ' ';
+			sh++;
+		}
+		n = n+1;
+	}
+		for (i = 0; i < len; i++)
+			input[i] = temp[i];
 }
--- src/commonWords.cpp
+++ src/commonWords.cpp
@@ -17,5 +17,34 @@ NOTES: If there are no common words return NULL.
 #define SIZE 31
 
 char ** commonWords(char *str1, char *str2) {
+	char **common;
+	common = (char**)malloc(sizeof(char) * SIZE);
+	int i, j,st1=0,st2=0,flag;
+	if (str1 && str2)
+	{
+		//i = 0; j = 0;
+		while (str1[i] && str2[j])
+		{
+			flag = 0;
+			for (i = st1,j=st2; str1[i] && str2[j] && str1[i] != ' ' && str2[j]!=' '; i++,j++)
+			{
+				if (str1[i] != str2[i])
+				{
+					break; flag = 1;
+				}
+			}
+			if (flag)
+			{
+				for (st1 = i; str1[st1] != ' '; st1++);
+			}
+			else{
+
+				for (st1 = i; str1[st1] != ' '; st1++);
+				for (st2 = j; str2[st2] != ' '; st2++);
+			}
+			st1++;
+		}
+
+	}
 	return NULL;
 }
